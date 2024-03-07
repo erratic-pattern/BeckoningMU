a515a5814878d3342904f0d3fca39bec97ecd952
Devil <69057727+DasBluEyedDevil@users.noreply.github.com>
2024-03-06 21:48:18 -0500

    Update vampcmdset.py

--- vampstatus/vampcmdset.py
+++ vampstatus/vampcmdset.py
@@ -3,11 +3,5 @@ from evennia import CmdSet
 class VampireCommandSet(CmdSet):
     key = "VampireCommandSet"
     def at_cmdset_creation(self):
-        self.add(CmdSetStatus())
-        self.add(CmdCheckStatus())
-        self.add(CmdSetAge())
-        self.add(CmdSetTitle())
-        self.add(CmdGrantStatusPoints())
-        self.add(CmdAdjustStatus())
-        self.add(CmdStatusBoard())
+
         pass
