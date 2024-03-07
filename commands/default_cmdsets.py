918e03c4bdd0ad4e2c47427b20c6637562fbafac
Devil <69057727+DasBluEyedDevil@users.noreply.github.com>
2024-03-06 21:30:10 -0500

    Update default_cmdsets.py

--- commands/default_cmdsets.py
+++ commands/default_cmdsets.py
@@ -24,21 +24,7 @@ from .notes import cmdNotes, CmdNoteApprove, CmdNoteProve
 from .dice import dice
 from evennia.contrib.game_systems.multidescer import CmdMultiDesc 
 from evennia import CmdSet
-from commands.vampstatus import CmdSetStatus, CmdCheckStatus, CmdSetAge, CmdSetTitle, CmdGrantStatusPoints, CmdAdjustStatus, CmdStatusBoard
 
-class VampireCommandSet(CmdSet):
-    key = "VampireCommandSet"
-
-    def at_cmdset_creation(self):
-        self.add(CmdSetStatus())
-        self.add(CmdCheckStatus())
-        self.add(CmdSetAge())
-        self.add(CmdSetTitle())
-        self.add(CmdGrantStatusPoints())
-        self.add(CmdAdjustStatus())
-        self.add(CmdStatusBoard())
-        self.add(CmdRouse())
-        self.add(CmdSlake())
 
 class CharacterCmdSet(default_cmds.CharacterCmdSet):
     """
